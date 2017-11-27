62e11252ab44de83a667e12768cf95e5354af0bf
vilmar <vilmarmoreira2010@gmail.com>
2017-11-27 02:18:29 -0200

    novos
    commit em massa

---
+++ c++/calculadora_18.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+int main(){
+
+int num1,num2, soma,sub,multi,div;
+
+cout<<"seja bem vindo!!!!!!!"<<endl;
+cout<<"digite o primeiro numero"<<endl;
+cin>>num1;
+cout<<"digite o segundo numero"<<endl;
+cin>>num2;
+soma=num1+num2;
+sub=num1-num2;
+multi=num1*num2;
+div=num1/num2;
+
+cout <<"a soma e: "<<soma<<endl;
+cout <<"a subtraçao e: "<<sub<<endl;
+cout <<"a multiplicaçao e: "<<multi<<endl;
+cout <<"a divisao e: "<<div<<endl;
+
+system("pause");
+return 0;
+}
