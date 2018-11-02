23eb570f04e3f3c0b9b4f516d30c4d706d535b56
vilmar-moreira-souza <vilmarmoreira2010@gmail.com>
2018-11-02 18:45:03 -0300

    s code

--- python/Chatbot/venv/Chatbot_one.py
+++ python/Chatbot/venv/Chatbot_one.py
@@ -1,13 +1,11 @@
+#chatbot com python
+
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
-
 bot = ChatBot('TW Chat Bot')
-
 conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python']
-
 bot.set_trainer(ListTrainer)
 bot.train(conversa)
-
 while True:
     pergunta = input("Usuário: ")
     resposta = bot.get_response(pergunta)
